1d122fdcef3b94ee930bb624e0fa2e48411bb7ab
Masaya Kinefuchi <kine0718@icloud.com>
2023-05-19 09:05:28 +0900

    パス修正

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Nav from './Nav.jsx'
 import Hero from './Hero.jsx'
-import About from './about.jsx'
+import About from './About.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
