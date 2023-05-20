0a8d06aea72e98e7df3cbcd5ad3e79f203033e59
Masaya Kinefuchi <kine0718@icloud.com>
2023-05-21 06:51:46 +0900

    パス変更

--- src/Reseach.jsx
+++ src/Reseach.jsx
@@ -5,6 +5,7 @@ function Reseach() {
 
   return (
     <>
+ 
       <div
         id="reseach"
         className="container mt-20 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full"
@@ -12,7 +13,7 @@ function Reseach() {
         <section className="w-full">
           <h1 className="secondary-title mb-6">Reseach</h1>
           <div className="reseach-img relative flex flex-col justify-center  md:w-3/4  h-auto mx-auto">
-            <img src="../images/reserch.png" className="w-full" alt="" />
+            <img src="https://github.com/kinemasa/myportfolio/blob/main/images/reserch.png?raw=true" className="w-full" alt="" />
             <div className="bg-black opacity-70 h-full w-1/3 absolute top-0 left-0 flex flex-col items-center">
               <h1 className="text-2xl underline mt-5 sm:text-2xl sm:mt-10 md:mt-20 xl:mt-32 xl:text-5xl">
                 Reseach
@@ -22,6 +23,7 @@ function Reseach() {
                 <span className="hidden sm:block">
                   特に眼底の画像情報と顔画像情報の比較を行うことで眼底検査の負担軽減を目指す。
                 </span>
+                
               </p>
               <button className="p-4 border-2 mt-4 rounded-md hover:bg-white hover:text-black"> Read More</button>
              
